72112425aa8c783dadb1eda115d124dd4960c7da
kalubekalu <kalubekalu1@gmail.com>
2022-11-24 19:26:25 +0300

    added mongo db configs

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 
+const db = require("./db");
+
 const app = express();
 const apiPort = 5000;
 
