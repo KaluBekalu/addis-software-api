4b378e33e401017e785825c42562daf2bd5b6ad8
kalubekalu <kalubekalu1@gmail.com>
2022-11-24 19:33:18 +0300

    dockerize

--- db/index.js
+++ db/index.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 
+// configure this connection string with respext docker mongo image
 const connectionString = "mongodb://mongo:27017/addis-soft";
 
 mongoose.connect(connectionString, { useNewUrlParser: true }).catch((e) => {
