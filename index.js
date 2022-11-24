786dde0980909ca78dbf54e1f4f059468ab9178f
kalubekalu <kalubekalu1@gmail.com>
2022-11-24 19:25:17 +0300

    added basic express configs

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
 const app = express();
 const apiPort = 5000;
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
 
 app.get("/", (req, res) => {
   res.send("Hello Addis soft!");
